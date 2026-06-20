8439307671621856d4f88f52186d23969f415b31
Karthik Chowdary <karthikchowdary2410@gmail.com>
2026-06-20 16:33:57 +0530

    Fix database initialization on Render

--- app.py
+++ app.py
@@ -179,8 +179,8 @@ def upload_pdf():
         }), 500
 
 # Run application
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
+if __name__ == "__main__":
     app.run(debug=True)
